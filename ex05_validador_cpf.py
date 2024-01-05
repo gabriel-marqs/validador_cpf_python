5ba09530bb28f24e3ec560a123657f7519ed9d6b
Gabriel Marques <gabriel.marq94@gmail.com>
2024-01-05 18:57:32 -0300

    Adicionado validador do segundo dígito do CPF, e otimizado tratamento de tipos das variáveis

--- ex05_validador_cpf.py
+++ ex05_validador_cpf.py
@@ -1,22 +1,30 @@
 cpf = input('Informe o CPF: ')
 
 digitos_separados = [char for char in cpf if char.isdigit()]
-tamanho_lista = len(digitos_separados)
 
-while tamanho_lista != 11:
+while len(digitos_separados) != 11:
     cpf = input('CPF inválido, digite novamente: ')
     digitos_separados = [char for char in cpf if char.isdigit()]
-    tamanho_lista = len(digitos_separados)
 
+digitos_separados = digitos_separados[:9]
 total = 0
 
-for indice, digito in enumerate(digitos_separados[:9], start=0):
-    total += int(digito) * (10 - indice)    
-
+for indice, digito in enumerate(digitos_separados, start=0):
+    total += int(digito) * (10 - indice)
+    
 digito_1 = (total * 10) % 11
-digito_1 = 0 if digito_1 > 9 else digito_1
+digito_1 = str(0 if digito_1 > 9 else digito_1)
 
-if digito_1 == int(digitos_separados[9]):
-    print('1º dígito do CPF válido')
+digitos_separados.append(digito_1)
+
+digito_2 = 0
+for indice, digito in enumerate(digitos_separados, start=0):
+    digito_2 += int(digito) * (11 - indice)
+
+digito_2 = (digito_2 * 10) % 11
+digito_2 = str(0 if digito_2 > 9 else digito_2)
+
+if cpf[9] == digito_1 and cpf[10] == digito_2:
+    print('CPF válido')
 else:
-    print('1º dígito do CPF inválido')
+    print('CPF inválido')
