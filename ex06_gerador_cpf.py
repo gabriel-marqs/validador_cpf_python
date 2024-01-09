0a77b9056b9d8e596249b7f355054ab1c9efdcaf
Gabriel Marques <gabriel.marq94@gmail.com>
2024-01-09 19:45:37 -0300

    Estruturando codigo de forma mais organizada

--- ex06_gerador_cpf.py
+++ ex06_gerador_cpf.py
@@ -1,13 +1,13 @@
 import random
 
-cpf = []
-
 def gerador_digito(cpf):
     digito = 0
     for indice, numero in enumerate(cpf, start=0):
         digito += int(numero) * (len(cpf) + 1 - indice)
     digito = (digito * 10) % 11
     cpf.append(str(0 if digito > 9 else digito))
+
+cpf = []
 
 for digito in range(9):
     numero = str(random.randint(0, 9))
