8382e701558bcc5014a4394162d10c4d3a6d2f34
Gabriel Marques <gabriel.marq94@gmail.com>
2024-01-09 19:36:53 -0300

    Usando função no gerador de cpf

--- ex06_gerador_cpf.py
+++ ex06_gerador_cpf.py
@@ -2,32 +2,17 @@ import random
 
 cpf = []
 
+def gerador_digito(cpf):
+    digito = 0
+    for indice, numero in enumerate(cpf, start=0):
+        digito += int(numero) * (len(cpf) + 1 - indice)
+    digito = (digito * 10) % 11
+    cpf.append(str(0 if digito > 9 else digito))
+
 for digito in range(9):
     numero = str(random.randint(0, 9))
     cpf.append(numero)
 
-
-digito_1 = 0
-contador = len(cpf) + 1
-
-for indice, digito in enumerate(cpf, start=0):
-    digito_1 += int(digito) * (contador - indice)
-    
-digito_1 = (digito_1 * 10) % 11
-digito_1 = str(0 if digito_1 > 9 else digito_1)
-
-cpf.append(digito_1)
-
-digito_2 = 0
-contador = len(cpf) + 1
-
-for indice, digito in enumerate(cpf, start=0):
-    digito_2 += int(digito) * (contador - indice)
-
-digito_2 = (digito_2 * 10) % 11
-digito_2 = str(0 if digito_2 > 9 else digito_2)
-
-cpf.append(digito_2)
-cpf = ''.join(cpf)
-
-print(cpf)
+gerador_digito(cpf)
+gerador_digito(cpf)
+print(''.join(cpf))
